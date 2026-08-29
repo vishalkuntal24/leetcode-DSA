b771c608c1ded39d13d7668e288bd594f4128062
vishalkuntal24 <vishalkuntal190@gmail.com>
2026-08-29 20:02:53 +0530

    Time: 0 ms (100%), Space: 10.2 MB (75.41%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,98 +5,23 @@ public:
         int sum=0 ;
         int i=0 ;
 
-        if(s.empty()) return 0 ;
-
-        while(s[i+1]!='\0'&&s[i]!='\0')
+        static const unordered_map<char, int> map = 
         {
-            if(s[i]=='I')
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        while(s[i]!='\0')
+        {
+            if(s[i+1]!='\0')
             {
-                if(s[i+1]=='V') 
-                {
-                    sum += 4 ;
-                    i++ ;
-                }
-               
-                else if(s[i+1]=='X') 
-                {
-                    sum += 9 ;
-                    i++ ;
-                }
-                else sum += 1 ;
+                if(map.at(s[i])<map.at(s[i+1])) sum-=map.at(s[i]) ;
+                else sum+=map.at(s[i]) ;
             }
-
-            else if(s[i]=='X')
-            {
-                if(s[i+1]=='L') 
-                {
-                    sum += 40 ;
-                    i++ ;
-                }
-               
-                else if(s[i+1]=='C') 
-                {
-                    sum += 90 ;
-                    i++ ;
-                }
-
-                else sum += 10 ;
-            }
-
-            else if(s[i]=='C')
-            {
-                if(s[i+1]=='D') 
-                {
-                    sum += 400 ;
-                    i++ ;
-                }
-               
-                else if(s[i+1]=='M') 
-                {
-                    sum += 900 ;
-                    i++ ;
-                }
-                else sum+=100 ;
-            }
-
-            else if(s[i]=='M') sum += 1000 ;
-            else if(s[i]=='L') sum+=50 ;
-            else if(s[i]=='V') sum+=5 ;
-            else sum+=500 ;
-
+            else sum+=map.at(s[i]) ;
             i++ ;
         }
 
-        if(s[i]!='\0')
-        {
-                switch(s[i])
-            {
-                case 'I' :
-                        sum+=1 ;
-                        break ;
-
-                case 'V' :
-                        sum+=5 ;
-                        break ;
-                case 'X' :
-                        sum+=10 ;
-                        break ;
-
-                case 'L' :
-                        sum+=50 ;
-                        break ;
-
-                case 'C' :
-                        sum+=100 ;
-                        break ;
-
-                case 'D' :
-                        sum+=500 ;
-                        break ;
-                default : sum += 1000 ;
-            }
-
-        }
-        
         return sum ;
     }
 };
